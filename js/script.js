a946c80630d2fd6b4dbc718ce7bfc4b495683a6d
Mileto Di Marco <mileto.di.marco@howest.be>
2021-09-14 15:15:26 +0200

    Fix conventions

--- js/script.js
+++ js/script.js
@@ -1,155 +1,149 @@
 "use strict";
 
-window.addEventListener('load',Initieer);
+window.addEventListener("load",initialize);
 
-let txtIngave, txaResult; 
-let slcTalen;
+let inpInput, txaResult; 
+let slcLanguages;
 let divFeedback;
-let slcHoofd,slcDetail;
-let slcGenodigden,slcDeelnemers;
-let cbxSporten;
-let divSporten;
-let rdbGeslacht;
-let divToonGeslacht;
-let frmInvul;
+let slcHead,slcDetail;
+let slcInvites,slcParticipants;
+let ckbSports;
+let divSports;
+let rdbSex;
+let divShowSex;
+let frmFill;
 
-const detailInhoud = [['Krokus','Roos','Tulp'], ['Eik','Es','Populier'],['Aap', 'Beer','Hond','Schaap']];
+const detailContents = [["Krokus","Roos","Tulp"], ["Eik","Es","Populier"],["Aap", "Beer","Hond","Schaap"]];
 
 
-function Initieer() {
-  let btnToevoegen;
-  let btnToonEerste,btnToonGeselecteerd,btnToonAantal;
-  let btnHeen, btnTerug;
-  let btnToonGeslacht;
+function initialize() {
   //  DOM elementen ophalen
-  txtIngave = document.getElementById("ingave");
+  inpInput = document.getElementById("input");
   txaResult = document.getElementById("result");
-  slcTalen = document.querySelector('#talen');
-  slcHoofd = document.querySelector('#hoofd');
-  slcGenodigden = document.querySelector('#genodigden');
-  slcDeelnemers = document.querySelector('#geelnemers');
-  slcDetail = document.querySelector('#detail');
-  divFeedback = document.querySelector('#feedback');
-  divSporten = document.querySelector('#sporten');
-  divToonGeslacht = document.querySelector('#toon-geslacht');
-  btnToevoegen = document.querySelector("#toevoegen");
-  btnToonEerste = document.querySelector('#toon-eerste');
-  btnToonGeselecteerd = document.querySelector('#toon-geselecteerd');
-  btnToonAantal = document.querySelector('#toon-aantal');
-  btnHeen = document.querySelector('#heen');
-  btnTerug = document.querySelector('#terug');
-  btnToonGeslacht = document.querySelector('#toon-geslacht');
-  cbxSporten = document.querySelectorAll('input[type="checkbox"]');
-  rdbGeslacht = document.getElementsByName('geslacht');
-  frmInvul = document.querySelector('#invul');
+  slcLanguages = document.querySelector("#languages");
+  slcHead = document.querySelector("#head");
+  slcInvites = document.querySelector("#invites");
+  slcParticipants = document.querySelector("#participants");
+  slcDetail = document.querySelector("#detail");
+  divFeedback = document.querySelector("#feedback");
+  divSports = document.querySelector("#sports");
+  divShowSex = document.querySelector("#show-sex");
+  const btnAdd = document.querySelector("#add");
+  const btnShowFirst = document.querySelector("#show-first");
+  const btnShowSelected = document.querySelector("#show-selected");
+  const btnShowQuantity = document.querySelector("#show-number-of");
+  const btnBack = document.querySelector("#away");
+  const btnAway = document.querySelector("#back");
+  const btnShowSex = document.querySelector("#show-sex");
+  ckbSports = document.querySelectorAll("input[type='checkbox']");
+  rdbSex = document.getElementsByName("sex");
+  frmFill = document.querySelector("#fill");
   // Eventlisteners toevoegen
-  btnToevoegen.addEventListener('click', vulTextArea);
-  btnToonEerste.addEventListener('click', toonEerste);
-  btnToonGeselecteerd.addEventListener('click', toonGeselecteerd);
-  btnToonAantal.addEventListener('click', toonAantal);
-  btnHeen.addEventListener('click',()=> doorgeef(genodigden, deelnemers));
-  btnTerug.addEventListener('click', ()=> doorgeef(deelnemers, genodigden));
-  btnToonGeslacht.addEventListener('click', toonGeslacht);
-  slcHoofd.addEventListener('change', toonDetaillijst);
-  slcDetail.addEventListener('dblclick', verwijderElement);
+  btnAdd.addEventListener("click", fillTextArea);
+  btnShowFirst.addEventListener("click", showFirst);
+  btnShowSelected.addEventListener("click", showSelected);
+  btnShowQuantity.addEventListener("click", showNumberOfElements);
+  btnBack.addEventListener("click",()=> pass(slcInvites, slcParticipants));
+  btnAway.addEventListener("click", ()=> pass(slcParticipants, slcInvites));
+  btnShowSex.addEventListener("click", showSex);
+  slcHead.addEventListener("change", showDetailList);
+  slcDetail.addEventListener("dblclick", removeElement);
   
-
-  voegEventlistenersToeAanCheckboxen();
+  AddEventListenersToCheckboxes();
 }
 
-function controle() {
-  if(frmInvul.naam.value == '' || frmInvul.voornaam.value == ''){
-    alert('Naam en voornaam invullen aub');
+function check() {
+  if(frmFill.lastname.value == "" || frmFill.firstname.value == "") {
+    alert("Naam en voornaam invullen aub");
     return false;
   }
   return true;
-
 }
 
-function toonGeslacht() {
+function showSex() {
   let retourString = "Gekozen geslacht : ";
-  for (let i=0; i<rdbGeslacht.length; i++) {
-    if (rdbGeslacht[i].checked) {
-      retourString += rdbGeslacht[i].value;
+  for (let i=0; i<rdbSex.length; i++) {
+    if (rdbSex[i].checked) {
+      retourString += rdbSex[i].value;
       break;
     }
   }
-  divToonGeslacht.innerHTML = retourString;
+  divShowSex.innerHTML = retourString;
 }
 
-function voegEventlistenersToeAanCheckboxen() {
-  for (let i = 0 ; i<cbxSporten.length; i++) {
-    cbxSporten[i].addEventListener('change', vulDivSporten);
+function AddEventListenersToCheckboxes() {
+  for (let i = 0 ; i<ckbSports.length; i++) {
+    ckbSports[i].addEventListener("change", fillDivSports);
   }
 
 }
 
-function vulDivSporten() {
-  let retour = 'Je verkoos de sporten : ';
-  for (let i = 0 ; i<cbxSporten.length; i++) {
-    if (cbxSporten[i].checked) {
-      retour += cbxSporten[i].value + ' ';
+function fillDivSports() {
+  let retour = "Je verkoos de sporten : ";
+  for (let i = 0 ; i<ckbSports.length; i++) {
+    if (ckbSports[i].checked) {
+      retour += ckbSports[i].value + " ";
     }    
   }
-  divSporten.innerHTML = retour;
+  divSports.innerHTML = retour;
 
 }
 
-function doorgeef(van,naar) {
-  let vanLengte = van.length;
+function pass(from,to) {
+  let fromLengte = from.length;
 
-  // Geselecteerden toevoegen aan 'naar'
-  for(let i = 0; i<vanLengte; i++) {
-    if(van[i].selected) {
-      naar[naar.length] = new Option(van[i].text, van[i].value);
+  // Geselecteerden toevoegen aan "naar"
+  for(let i = 0; i<fromLengte; i++) {
+    if(from[i].selected) {
+      to[to.length] = new Option(from[i].text, from[i].value);
     }
   }
 
-  // Geselecteerden in 'van' verwijderen
-  for (let i=(vanLengte-1); i>=0; i--) {
-    if(van[i].selected) {
-      van[i] = null;
+  // Geselecteerden in "van" verwijderen
+  for (let i=(fromLengte-1); i>=0; i--) {
+    if(from[i].selected) {
+      from[i] = null;
     }
   }
 
 }
 
-function toonDetaillijst() {
-  let geselecteerdItem = slcHoofd.selectedIndex;
+function showDetailList() {
+  const selectedItem = slcHead.selectedIndex;
   // Leegmaken van de detailLijst
   slcDetail.length = 0;
   // Opvullen van de detailLijst
-  for(let i=0; i< detailInhoud[geselecteerdItem].length;i++) {
-    slcDetail[slcDetail.length]= new Option(detailInhoud[geselecteerdItem][i]);
+  for(let i=0; i< detailContents[selectedItem].length;i++) {
+    slcDetail[slcDetail.length]= new Option(detailContents[selectedItem][i]);
   }
   // Eerste item selecteren uit de lijst
   slcDetail[0].selected = true;
 }
 
-function verwijderElement() {
+function removeElement() {
   slcDetail[slcDetail.selectedIndex] = null;
 }
 
-function vulTextArea() {
-    let invoerwaarde = txtIngave.value;
-    txaResult.value += invoerwaarde + '\n';
-    txtIngave.value = '';
+function fillTextArea() {
+    let input = inpInput.value;
+    txaResult.value += input + "\n";
+    inpInput.value = "";
 }
 
-function toonEerste() {
-  divFeedback.innerHTML='';
-  let eersteTaal = slcTalen.options[0];
-  // OF slcTalen[0]
-  divFeedback.innerHTML = `Eerste taal = ${eersteTaal.text} met value ${eersteTaal.value}`;
+function showFirst() {
+  divFeedback.innerHTML="";
+  let firstLanguage = slcLanguages.options[0];
+  // OF slcLanguages[0]
+  divFeedback.innerHTML = `Eerste taal = ${firstLanguage.text} met value ${firstLanguage.value}`;
 }
 
-function toonGeselecteerd() {
-  divFeedback.innerHTML='';
-  let geselecteerdeTaal = slcTalen.options[slcTalen.selectedIndex]; // OF slcTalen[slcTalen.selectedIndex]
-  divFeedback.innerHTML = `Gekozen taal = ${geselecteerdeTaal.text} met value ${geselecteerdeTaal.value}`;
+function showSelected() {
+  divFeedback.innerHTML="";
+  const selectedLanguage = slcLanguages.options[slcLanguages.selectedIndex]; // OF slcLanguages[slcLanguages.selectedIndex]
+  divFeedback.innerHTML = `Gekozen taal = ${selectedLanguage.text} met value ${selectedLanguage.value}`;
 }
 
-function toonAantal() {
-  divFeedback.innerHTML='';
-  divFeedback.innerHTML = `Aantal elementen : ${slcTalen.length} ` // OF slcTalen.options.length ;
+function showNumberOfElements() {
+  divFeedback.innerHTML="";
+  divFeedback.innerHTML = `Aantal elementen : ${slcLanguages.length} ` // OF slcLanguages.options.length ;
 }
